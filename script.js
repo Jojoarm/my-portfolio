7e730876bf07348b69c76e58789f5ea03d89dff8
George Ofogba <ofogbageorge3@gmail.com>
2021-05-06 00:27:33 +0100

    about section added

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const navMenu = document.querySelector('.menu')
+const menuBtn = document.querySelector('#menu-btn')
+const menuClose = document.querySelector('.menu-btn i')
+
+// toggle menu/navbar script
+menuBtn.addEventListener('click', ()=> {
+    navMenu.classList.toggle('active')
+    menuClose.classList.toggle('active')
+})
+
+
 // typing text animation script
 var typed = new Typed(".typing", {
     strings: ["A Front End Web Developer", "An Aspiring Full Stack Developer", "An Engineer", "A Designer", "A Freelancer"],
