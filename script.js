b4006220fea4c08c293e1b960e24286881598216
George Ofogba <ofogbageorge3@gmail.com>
2021-05-05 08:30:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["A Front End Web Developer", "An Aspiring Full Stack Developer", "An Engineer", "A Designer", "A Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["A Front End Web Developer", "An Aspiring Full Stack Developer", "An Engineer", "A Designer", "A Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
