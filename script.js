c33b3697965678b917b7c4df5511b4d233d4a7fe
George Ofogba <ofogbageorge3@gmail.com>
2021-05-06 18:03:38 +0100

    project section created

--- script.js
+++ script.js
@@ -23,3 +23,27 @@ var typed = new Typed(".typing-2", {
     backSpeed: 60,
     loop: true
 });
+
+
+// owl carousel script
+$('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    center: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 1,
+            nav: false
+        },
+        1000:{
+            items: 1,
+            nav: false
+       }
+    }
+});
